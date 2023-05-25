fe22709256573af5301f22ea6aff7f9c2d423cb9
CalaMiTY0311 <axelsilas12@naver.com>
2023-05-25 21:27:36 +0900

    re push cats_V_dogs

---
+++ ImageDataSetting.py
@@ -0,0 +1,55 @@
+from keras.preprocessing.image import ImageDataGenerator
+from PIL import Image
+from dogsVcats_copy import copy_train_path
+from keras_model import image_height, batch_size, input_shape, epochs, cnn_api
+from make_callback import callbacks
+import os
+
+
+train_datagen = ImageDataGenerator(rescale = 1./255)
+val_datagen = ImageDataGenerator(rescale = 1./255) # 검증데이터 스케일 조정만 합니다.
+
+train_generator = train_datagen.flow_from_directory(
+    os.path.join(copy_train_path,"train"),
+    target_size = (image_height, image_height),
+    batch_size = batch_size,
+    class_mode = "binary"
+    )
+validation_generator = val_datagen.flow_from_directory(
+    os.path.join(copy_train_path,"validation"),
+    target_size = (image_height, image_height),
+    batch_size = batch_size,
+    class_mode = "binary"
+    )
+
+#gpu가 두개로 병렬로 각각 다른 작업을 원하시면
+# with를 사용하시고 gpu:0 , gpu:1 이러게 모델을 지정하시면 됩니다. 
+# from tensorflow as tf
+# with tf.device('gpu:0'):
+#     newType_model = cnn_api(input_shape)
+    
+newType_model = cnn_api(input_shape)
+hist = newType_model.fit_generator(train_generator, steps_per_epoch = 20000//batch_size, epochs= epochs,
+                                  validation_data = validation_generator, validation_steps = 5000//batch_size,
+                                  callbacks = callbacks)
+
+import matplotlib.pyplot as plt
+train_acc = hist.history['acc']
+val_acc = hist.history['val_acc']
+
+train_loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+epochs = range(1,len(train_acc)+1)
+
+plt.plot(epochs,train_acc,'bo',label='Training acc')
+plt.plot(epochs,val_acc,'r',label='Val acc')
+plt.title('Training and Val accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs,train_loss,'bo',label='Training loss')
+plt.plot(epochs,val_loss,'r',label='Val loss')
+plt.title('Training and Val loss')
+plt.legend()
+
+plt.show()
--- dogsVcats_copy.py
+++ dogsVcats_copy.py
@@ -2,7 +2,7 @@ import os, shutil
 
 original_dataset_path = './dogsVcats'
 copy_train_path = './datasets'
-
+""" 
 def copy_files(dogORcat_path,start_num,end_num,trainOrval_path):
     
     image_paths = [os.path.join(original_dataset_path,"train",dogORcat_path + '.' + str(i) + '.jpg')
@@ -16,6 +16,7 @@ def copy_files(dogORcat_path,start_num,end_num,trainOrval_path):
     for image_path in image_paths:
         shutil.copy(image_path, target_copy_paths)
 
+############################################################################################데이터 카피코드
 copy_files("dog",0,10000,"train")
 copy_files("cat",0,10000,"train")
 copy_files("dog",10000,12500,"validation")
@@ -25,3 +26,4 @@ print("훈련데이터 Dog 개수",len(os.listdir('./datasets/train/dog')))
 print("훈련데이터 Cat 개수",len(os.listdir('./datasets/train/cat')))
 print("검증데이터 Dog 개수",len(os.listdir('./datasets/validation/dog')))
 print("검증데이터 Cat 개수",len(os.listdir('./datasets/validation/cat')))
+"""
